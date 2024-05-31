09b42094c72c215bf3d769a3048704d595b44964
MrMaratuk <maratik1036@gmail.com>
2024-05-31 14:23:08 +0300

    .env created

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 from aiogram import Bot, Dispatcher
-from config import TOKEN
 from aiogram.enums import ParseMode
 from aiogram.client.bot import DefaultBotProperties
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.environ["TOKEN"]
+
 
 # Подключение бота к АПИ
 bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
